a80e61239b0f7f613bfc16f2a1af628f58888706
shkshitiz <shkshitiz1@gmail.com>
2022-09-14 15:38:11 +0930

    heroku fix

--- db/db.rb
+++ db/db.rb
@@ -1,16 +1,16 @@
 require 'pg'
 
-def run_sql(sql, sql_params = [])
-    db = PG.connect(dbname: 'fantasy_heart_db')
-    results = db.exec_params(sql, sql_params)
-    db.close
-    results
-end
-
-
 # def run_sql(sql, sql_params = [])
-#     PG.connect(ENV['DATABASE_URL'] || {dbname: 'fantasy_heart_db'})    
+#     db = PG.connect(dbname: 'fantasy_heart_db')
 #     results = db.exec_params(sql, sql_params)
 #     db.close
 #     results
 # end
+
+
+def run_sql(sql, sql_params = [])
+    db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'fantasy_heart_db'})    
+    results = db.exec_params(sql, sql_params)
+    db.close
+    results
+end
